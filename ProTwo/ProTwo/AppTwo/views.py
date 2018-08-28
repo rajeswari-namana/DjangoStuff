882741d76e6527cb4d95510106ae94c9a165061e
Rajeswari Devi Namana <raji.namana@gmail.com>
2018-08-28 13:26:10 -0500

    added new /help route

--- ProTwo/ProTwo/AppTwo/views.py
+++ ProTwo/ProTwo/AppTwo/views.py
@@ -5,3 +5,6 @@ def index(request):
     return HttpResponse("<em>My Second App</em>")
 
 # Create your views here.
+def help(request):
+    dict_two={'help_text':"hey need some help"}
+    return render(request,"AppTwo/index.html",context=dict_two)
--- first_project/first_app/views.py
+++ first_project/first_app/views.py
@@ -3,6 +3,6 @@ from django.http import HttpResponse
 
 def index(request):
     my_dict={'insert_me':"hello i am from views.py"}
-    return render(request,"index.html",context=my_dict)
+    return render(request,"first_app/index.html",context=my_dict)
 
 # Create your views here.
