2ec8f1b085639d5f084901185fbdb117ea7067e0
Rajeswari Devi Namana <raji.namana@gmail.com>
2018-08-28 12:44:54 -0500

    adding include function & creating template

--- first_project/first_app/views.py
+++ first_project/first_app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello")
+    my_dict={'insert_me':"hello i am from views.py"}
+    return render(request,"index.html",context=my_dict)
 
 # Create your views here.
