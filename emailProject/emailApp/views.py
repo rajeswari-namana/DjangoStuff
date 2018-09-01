397db317f44df0a5491754838175a201a56179e8
Rajeswari Devi Namana <raji.namana@gmail.com>
2018-09-01 10:53:45 -0500

    added forms
    for user input to be saved to data base

--- emailProject/emailApp/views.py
+++ emailProject/emailApp/views.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render
-from emailApp.models import User
+#from emailApp.models import User
+from emailApp.forms import NewUserForm
 
 # Create your views here.
 def index(request):
     return render(request,"emailApp/index.html")
 
 def index1(request):
-    userDetails = User.objects.order_by('FirstName')
-    my_dict={'details':userDetails}
-    return render(request,"emailApp/users.html",context=my_dict)
+    form=NewUserForm()
+
+    if request.method=="POST":
+        form=NewUserForm(request.POST)
+
+        if form.is_valid():
+            form.save(commit=True)
+            return index(request)
+        else:
+            print("Form invalid")
+
+    return render(request,'emailApp/users.html',{'form':form})
