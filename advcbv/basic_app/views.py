e4b9bfe6e5748aa53efa0702e4a6ed4f8be1e74b
Rajeswari Devi Namana <raji.namana@gmail.com>
2018-09-03 10:26:13 -0500

    injecting context in templateView

--- advcbv/basic_app/views.py
+++ advcbv/basic_app/views.py
@@ -5,3 +5,8 @@ from django.views.generic import View,TemplateView
 # Create your views here.
 class IndexView(TemplateView):
     template_name='index.html'
+
+    def get_context_data(self,**kwargs):
+        context=super().get_context_data(**kwargs)
+        context['injectme']='Base Injection'
+        return context
